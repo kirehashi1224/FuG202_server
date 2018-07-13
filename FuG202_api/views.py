d9aaaa8d7c6f858f7010f2c699febe2623b72bda
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-07-13 18:41:16 +0900

    ランダムで1件取得する機能を実装

--- FuG202_api/views.py
+++ FuG202_api/views.py
@@ -1,6 +1,9 @@
 import  django_filters
-from rest_framework import viewsets, filters
+import random
+from rest_framework import viewsets
 from django_filters import rest_framework as filters
+from rest_framework.views import APIView
+
 
 from .models import Restaurant, Genre
 from .serializer import RestaurantSerializer, GenreSerializer
@@ -9,11 +12,24 @@ class RestaurantFilter(filters.FilterSet):
     # フィルタの定義
     name = filters.CharFilter(name="name", lookup_expr='contains')
     address = filters.CharFilter(name="address", lookup_expr='contains')
+    random_extract = filters.CharFilter(method='filter_random_extract')
+
+    def filter_random_extract(self, qs, name, value):
+        try:
+            if value == "true":
+                qs_len = len(qs)-1
+                index = random.randint(0, qs_len)
+                return qs[index:index+1]
+            else:
+                return qs
+        except:
+            return qs
+
     class Meta:
         model = Restaurant
         # フィルタを列挙する。
         # デフォルトの検索方法でいいなら、モデルフィールド名のフィルタを直接定義できる。
-        fields = ['name', 'address']
+        fields = ['name', 'address', 'random_extract']
 
 
 class RestaurantViewSet(viewsets.ModelViewSet):
