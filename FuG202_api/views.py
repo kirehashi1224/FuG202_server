30db4e48cec50bb6d02f58f9761ca78e36206a00
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-07-13 16:36:18 +0900

    レストランの部分一致検索を実装

--- FuG202_api/views.py
+++ FuG202_api/views.py
@@ -1,13 +1,25 @@
 import  django_filters
 from rest_framework import viewsets, filters
+from django_filters import rest_framework as filters
 
 from .models import Restaurant, Genre
 from .serializer import RestaurantSerializer, GenreSerializer
 
+class RestaurantFilter(filters.FilterSet):
+    # フィルタの定義
+    name = filters.CharFilter(name="name", lookup_expr='contains')
+    address = filters.CharFilter(name="address", lookup_expr='contains')
+    class Meta:
+        model = Restaurant
+        # フィルタを列挙する。
+        # デフォルトの検索方法でいいなら、モデルフィールド名のフィルタを直接定義できる。
+        fields = ['name', 'address']
+
+
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
-    filter_fields = ('name', 'address')
+    filter_class = RestaurantFilter
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
