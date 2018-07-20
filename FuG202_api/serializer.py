d2cd86624e301092b10fe1d32dcc6ffa89a343cd
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-07-20 17:54:51 +0900

    restaurantのクエリの返り値についてidも含めるように変更

--- FuG202_api/serializer.py
+++ FuG202_api/serializer.py
@@ -5,7 +5,7 @@ from .models import Restaurant, Genre
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ('name', 'address', 'timespans')
+        fields = ('id', 'name', 'address', 'timespans')
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
