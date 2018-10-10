9ef1b63b09b456ec18a59ab2b8e540d70b368d31
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-10-10 17:01:03 +0900

    /media/内にアップロードした画像をURL [IPアドレス]/media/[ファイル名]で表示できるように実装

--- FuG202_server/urls.py
+++ FuG202_server/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from FuG202_api.urls import router as fug202_api_router
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(fug202_api_router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
