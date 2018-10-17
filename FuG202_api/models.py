3c05d8c3ff9ccb6bfb0611c998f8660eeb95033f
masanoki <asano.m.af@m.titech.ac.jp>
2018-10-17 16:06:05 +0900

    データベースに画像のパスのカラムを追加

--- FuG202_api/models.py
+++ FuG202_api/models.py
@@ -4,6 +4,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     timespans = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
 
 class Genre(models.Model):
     name = models.CharField(max_length=20)
