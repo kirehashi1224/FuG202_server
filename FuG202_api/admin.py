4c73b5c082435aaf59b5565d0d0fb58ed21437bd
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-12-04 15:48:07 +0900

    Tagモデルについてをadmin.pyに追記

--- FuG202_api/admin.py
+++ FuG202_api/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Restaurant, Genre
+from .models import Restaurant, Tag, Genre
 
 
 @admin.register(Restaurant)
 class Restaurant(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class Tag(admin.ModelAdmin):
     pass
 
 
